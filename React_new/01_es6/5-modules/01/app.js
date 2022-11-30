b11460ead7ee436c774c63d261e26e9849a5595f
huudinh <dinhndvn@yahoo.com>
2022-11-30 14:43:19 +0700

    update

---
+++ React_new/01_es6/5-modules/01/app.js
@@ -0,0 +1,6 @@
+import logger from './logger.js';
+
+// console.log(typeof logger);
+// console.log(logger);
+// logger('Test message...', 'error');
+logger('Test message...', 'warn');
---
+++ React_new/01_es6/5-modules/03/app.js
@@ -0,0 +1,8 @@
+import logger from './logger.js';
+import {
+    TYPE_LOG,
+    TYPE_WARN,
+    TYPE_ERROR
+} from './constants.js';
+
+logger('Test message...', TYPE_WARN);
