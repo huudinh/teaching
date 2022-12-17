70c0c80b77eae7701713f6766e57bcc36e18c8cf
huudinh <dinhndvn@yahoo.com>
2022-12-17 11:30:54 +0700

    update

---
+++ React_new/01_es6/5-modules/05/app.js
@@ -0,0 +1,5 @@
+import logger from './logger/index.js';
+import * as constants from './constants.js';
+
+// console.log(constants);
+logger('Test message...', constants.TYPE_WARN);
---
+++ React_new/01_es6/5-modules/07/app.js
@@ -0,0 +1,5 @@
+import { logger2 } from './logger/index.js';
+import * as constants from './constants.js';
+
+// console.log(constants);
+logger2('Test message...', constants.TYPE_WARN);
