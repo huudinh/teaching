fdc121f98d750a370099a56a4498871c7176eac4
Triệu Hữu Định <trieuhuudinh@MacBook-Pro.local>
2024-01-13 23:03:47 +0700

    update

---
+++ Web_FullStack/session_1_hello_react/food-order/src/header.js
@@ -0,0 +1,10 @@
+import "./header.css";
+
+const Header = () => {
+    return (
+        <div className="app_header">
+            <span className="app_name">React Meals</span>
+            <button className="card_button">Your Card (0)</button>
+        </div>
+    )
+}
